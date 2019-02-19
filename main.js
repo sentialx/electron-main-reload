906561063169e414ef64df1a5699b8bafe838ceb
Sential <sentialx@gmail.com>
2019-02-19 14:29:29 +0100

    Code cleanups

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const { app } = require('electron');
-const chokidar = require('chokidar');
-const path = require('path');
+const { app } = require("electron");
+const chokidar = require("chokidar");
+const path = require("path");
 
 const appPath = app.getAppPath();
-const config = require(path.join(appPath, 'package.json'));
-const mainFile = path.join(appPath, config.main || 'index.js');
+const config = require(path.join(appPath, "package.json"));
+const mainFile = path.join(appPath, config.main || "index.js");
 
 module.exports = () => {
-  const watcher = chokidar.watch(mainFile);
+  const watcher = chokidar.watch(mainFile, { ignoreInitial: true });
 
-  watcher.on('change', () => {
+  watcher.on("change", () => {
     app.relaunch();
     app.exit();
   });
